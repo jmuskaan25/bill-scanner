2a0fb4fe9cd9bf9317efb6efd6fecea8e4f92c8c
Muskaan <muskaan@via.dev>
2026-04-07 14:35:40 +0530

    Add Firebase config (no secrets)

---
+++ config.js
@@ -0,0 +1,10 @@
+const CONFIG = {
+  FIREBASE: {
+    apiKey: 'AIzaSyCSsWOIVGvM35CxklXeqBUHOhZk12BwrEs',
+    authDomain: 'bill-scanner-ebaa8.firebaseapp.com',
+    projectId: 'bill-scanner-ebaa8',
+    storageBucket: 'bill-scanner-ebaa8.firebasestorage.app',
+    messagingSenderId: '301286509506',
+    appId: '1:301286509506:web:6ce16703a1ffcb38cfe45b'
+  }
+};
